d7a063817502b5051ff8120f9551b459cdfa77f4
varunjadav2002 <155519638+varunjadav2002@users.noreply.github.com>
2024-11-06 13:07:56 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Coin from './pages/Coin/Coin'
+import Footer from './components/Navbar/Footer/Footer'
 
 const App = () => {
-  return (
+  return ( 
     <div className='app'>
-      <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Cart' element={<Cart />} />
-          <Route path='/order' element={<PlaceOrder />} />
-        </Routes> 
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/coin/:coinId' element={<Coin/>}/>
+      </Routes>
+      <Footer/>
     </div>
   )
 }
 
 export default App
-
--- main.jsx
+++ main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import CoinContextProvider from './context/CoinContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <CoinContextProvider>
+      <App />
+    </CoinContextProvider>
+  </BrowserRouter>,
 )
-
-
-
-
-
